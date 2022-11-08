9698901ede5a2d222a8f02b304526d2f5ea47027
Koshin Valery <koshinva1993@gmail.com>
2022-11-08 12:51:43 +0300

    feat: настроить обработку ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,21 +1,26 @@
 const Card = require('../models/card');
+const { ERROR_CODE_400 } = require('../utils/errors/statusCode');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(500).json({ message: err.message }));
 };
 module.exports.addCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => {
+      res.status(ERROR_CODE_400).json({
+        message: 'Переданы некорректные данные в методы создания карточки',
+      });
+    });
 };
 module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
   Card.findByIdAndDelete(cardId)
     .then(() => res.send({ message: 'Карточка успешно удалена' }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(500).json({ message: err.message }));
 };
 module.exports.likeCard = (req, res) => {
   const { cardId } = req.params;
@@ -25,7 +30,7 @@ module.exports.likeCard = (req, res) => {
     { new: true },
   )
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(500).json({ message: err.message }));
 };
 module.exports.dislikeCard = (req, res) => {
   const { cardId } = req.params;
@@ -36,5 +41,5 @@ module.exports.dislikeCard = (req, res) => {
     { new: true },
   )
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(500).json({ message: err.message }));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,31 +1,52 @@
 const User = require('../models/user');
+const {
+  ERROR_CODE_400,
+  ERROR_CODE_404,
+  ERROR_CODE_500,
+} = require('../utils/errors/statusCode');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(ERROR_CODE_500).json({ message: err.message }));
 };
 module.exports.getUserById = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => {
+      res.status(ERROR_CODE_404).json({ message: 'Пользователь не найден' });
+    });
 };
 module.exports.addUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => {
+      res.status(ERROR_CODE_400).json({
+        message: 'Переданы некорректные данные в метод создания пользователя',
+      });
+    });
 };
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => {
+      res.status(ERROR_CODE_400).json({
+        message:
+          'Переданы некорректные данные в метод обновления профиля пользователя',
+      });
+    });
 };
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => {
+      res.status(ERROR_CODE_400).json({
+        message:
+          'Переданы некорректные данные в метод обновления аватара пользователя',
+      });
+    });
 };
