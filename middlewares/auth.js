0a7040701b5f55161330b15728842876cd95c227
Koshin Valery <koshinva1993@gmail.com>
2022-11-17 16:12:43 +0300

    feat: исправить ошибки после теста v2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,13 +4,13 @@ const UnsanctionedError = require('../utils/errors/unsanctionedError');
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    throw UnsanctionedError('Необходима авторизация');
+    return next(new UnsanctionedError('Необходима авторизация'));
   }
   let payload;
   try {
     payload = jwt.verify(token, '96a5281308389ea0b68de98da03e3c35');
   } catch (error) {
-    next(new UnsanctionedError('Необходима авторизация'));
+    return next(new UnsanctionedError('Необходима авторизация'));
   }
   req.user = payload;
   return next();
